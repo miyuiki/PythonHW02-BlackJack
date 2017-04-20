f97f06e6db6e815d590a51925bb93d2fb2159269
miyuiki <a121406@gmail.com>
2017-04-21 00:17:23 +0800

    ubuntu

--- client.py
+++ client.py
@@ -1,16 +1,58 @@
 import socket
+import sys
+import select
+import string
+ 
+##player = input("your name : ")
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Socket Created!")
+def prompt() :
+    sys.stdout.write("<PLAYER> ")
+    sys.stdout.flush()
 
-host = socket.gethostname()
-port = 9999
-
-client.connect((host,port))
-while True:
-	msg = input("send some message: ")
-	client.sendall(str(msg))
-	recv_data = client.recv(1024)
+#main function
+if __name__ == "__main__":
 	
-	print("echo from server is %s" % repr(recv_data))
-client.close()
+ 
+    if(len(sys.argv) < 3) :
+    	print 'Usage : python telnet.py hostname port'
+        sys.exit()
+ 
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+ 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(2)
+ 
+    # connect to remote host
+    try :
+        s.connect((host, port))
+    except :
+        print 'Unable to connect'
+        sys.exit()
+ 	
+    print 'Connected to remote host. Start sending messages'
+    prompt()
+ 
+    while 1:
+        socket_list = [sys.stdin, s]
+ 
+        # Get the list sockets which are readable
+        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+ 
+        for sock in read_sockets:
+            #incoming message from remote server
+            if sock == s:
+                data = sock.recv(4096)
+                if not data :
+                    print '\nDisconnected from game server'
+                    sys.exit()
+                else :
+                    #print data
+                    sys.stdout.write(data)
+                    prompt()
+ 
+            #user entered a message
+            else :
+                msg = sys.stdin.readline()
+                s.send(msg)
+                prompt()
--- server.py
+++ server.py
@@ -1,52 +1,74 @@
 import socket
 import select
-import sys
+import random
 
-HOST = ''
-PORT = 9999
-RECV_BUFFER = 4096
-socket_list = []
+card = []
+for i in xrange(1,5):
+    for j in xrange(1,14):
+        card.append((i,j))
+def get_card(card):
+    rand = random.randint(1, len(card)+1)
+    content = card[rand]
+    card.remove(content)
+    return content
+#Function to broadcast chat messages to all connected clients
+def broadcast_data (sock, message):
+    #Do not send the message to master socket and the client who has send us the message
+    for socket in CONNECTION_LIST:
+        try:
+            socket.send(message)
+        except :
+            pass
+ 
+if __name__ == "__main__":
+    houseman = []
+    player = []
 
-def BJ_server():
-
+    houseman.append(get_card(card))
+    houseman.append(get_card(card))
+    
+    # List to keep track of socket descriptors
+    CONNECTION_LIST = []
+    RECV_BUFFER = 4096 # Advisable to keep it as an exponent of 2
+    PORT = 5000
+ 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-    server_socket.bind((HOST,PORT))
-    server_socket.listen(10)
-    socket_list.append(server_socket)
-
-    print ("server socket start on port" + str(PORT))
-    while True:
-        ready_to_read, ready_to_write, in_error = select.select(socket_list, [], [], 0)
-        for sock in ready_to_read:
-            if socket == server_socket:  # new connection
-                socket_fd, adr = server_socket.accept()
-                socket_list.append(socket_fd)
-                print ("Client (%s,%s) connect" % adr)
-
-                broadcast(server_socket, socket_fd, "[%s:%s] entered game\n" % adr)
-            else:  # message from client
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.listen(5)
+    
+    # Add server socket to the list of readable connections
+    CONNECTION_LIST.append(server_socket)
+ 
+    print "Game server started on port " + str(PORT) + " waiting for player..."
+    print "card1: " + str(houseman[0]) + " card2: " + str(houseman[1])
+ 
+    while 1:
+        # Get the list sockets which are ready to be read through select
+        read_sockets,write_sockets,error_sockets = select.select(CONNECTION_LIST,[],[])
+ 
+        for sock in read_sockets:
+            #New connection
+            if sock == server_socket:
+                # Handle the case in which there is a new connection recieved through server_socket
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                print "Player (%s, %s) connected" % addr
+ 
+                broadcast_data(sockfd, "[%s:%s] join the game\n" % addr)
+                broadcast_data(sock, "house man: %s\n" %str(houseman[0]))
+ 
+            #Some incoming message from a client
+            else:
+                # Data recieved from client, process it
                 try:
                     data = sock.recv(RECV_BUFFER)
                     if data:
-                        broadcast(server_socket,sock,"\r"+'['+str(sock.getpeername())+']'+data)
-                    else:
-                        if sock in socket_list:
-                            socket_list.remove(sock)
-                        broadcast(server_socket,sock,"Client (%s,%s) is offline\n" % adr)
+                        broadcast_data(sock, "r" + ' ' + data)                
+ 
                 except:
-                    broadcast(server_socket, sock, "Client (%s,%s) is offline\n" % adr)
+                    broadcast_data(sock, "PLayer (%s, %s) is offline" % addr)
+                    print "Player (%s, %s) is offline" % addr
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
                     continue
-        server_socket.close()
-def broadcast(server_socket,sock,msg):
-    for socket in socket_list:
-        if socket != server_socket and socket != sock:
-            try:
-                socket.send(msg)
-            except:
-                socket.close()
-                if socket in socket_list:
-                    socket_list.remove(socket)
-
-if __name__ == '__main__':
-    sys.exit(BJ_server())
